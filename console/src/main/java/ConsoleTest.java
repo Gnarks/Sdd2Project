cb0924b34e089ae10ecc2efbe3580a67cec611df
Gnarks <231026@umons.ac.be>
2025-02-21 09:59:00 +0100

    changed tabs 4-> 2 in ConsoleTest

--- console/src/main/java/ConsoleTest.java
+++ console/src/main/java/ConsoleTest.java
@@ -1,14 +1,14 @@
 import java.io.IOException;
 import java.util.ArrayList;
 
-import shared.SceneReader;
 import shared.Segment;
 import shared.Example;
 
 public class ConsoleTest{
 
-    public static void main(String[] args){
-        System.out.println("yipie");
+  public static void main(String[] args){
+    System.out.println("yipie");
+
   }
 }
 
