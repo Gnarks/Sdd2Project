a2ecbdf475593f614289c3382489f5dd8dfcf81d
Gnarks <231026@umons.ac.be>
2025-03-20 14:44:49 +0100

    ups forgot this file

---
+++ shared/src/java/shared/DoubleUtils.java
@@ -0,0 +1,22 @@
+package shared;
+
+import java.lang.Math;
+
+public class DoubleUtils{
+  
+  /** returns if the doubles are equal with 1E-10 precision 
+ * @param x1
+ * @param x2
+ * @return
+   */
+  public static boolean areEqual(double x1, double x2){
+    return Math.abs(x1 - x2) < 1E-10;
+  }
+
+  /** returns if the doubles are lower or equal
+   * @see DoubleUtils.areEqual
+   */
+  public static boolean lowerOrEqual(double x1, double x2){
+    return areEqual(x1,x2) || x1 < x2;
+  }
+}
